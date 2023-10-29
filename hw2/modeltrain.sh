6438351b027160e98013125652bac26773a7d175
= <=>
2023-10-28 22:51:38 -0500

    add sh file for server job

---
+++ hw2/modeltrain.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --time=00:15:00   # walltime limit (HH:MM:SS)
+#SBATCH --nodes=1   # number of nodes
+#SBATCH --ntasks-per-node=1   # 8 processor core(s) per node 
+#SBATCH --gres=gpu:a100:1
+#SBATCH --partition=class-gpu    # gpu node(s)
+#SBATCH --job-name="dl2classa"
+#SBATCH --mail-user=sjx@iastate.edu   # email address
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+# LOAD MODULES, INSERT CODE, AND RUN YOUR PROGRAMS HERE
+module load r
+Rscript train2class.R
