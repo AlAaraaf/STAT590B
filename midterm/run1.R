d384dfb75f213e2200f683464dc60209bc7fcd3a
= <=>
2023-11-17 22:02:27 -0600

    update run1

--- midterm/run1.R
+++ midterm/run1.R
@@ -3,14 +3,12 @@ use_condaenv('connectr')
 library(tensorflow)
 library(keras)
 tf <- import('tensorflow')
-pil <- import('PIL')
 np <- import('numpy')
 source('midterm-code.R')
-
+setwd('/work/classtmp/carrice/STAT590B/midterm/')
 
 dataset = get.dataset1()
 layerlist = c(2,4,8,12,16)
-epochs = c(10,10,10,20,20)
 unitlists =list()
 unitlists[[1]] <- c(128,64) # 2 layers
 unitlists[[2]] <- c(128,64,64,32) # 4 layers
@@ -22,10 +20,12 @@ historys = list()
 lrlist = c(0.001, 0.01, 0.05)
 seedlist = c(1001, 1002, 1003, 1004, 1005)
 
-for (seed in seedlist){
-  set.seed(seed)
-  for (lr in lrlist){
-    for (i in 1:5){
+metric_record = c()
+for (lr in lrlist){
+  for (i in 1:5){
+    val_acc = c()
+    for(seed in seedlist){
+      set.seed(seed)
       model_name = paste(checkpoint_folder, 'q1_', i,'layer', '_',lr,'_',seed,sep = '')
       model_cp <- keras$callbacks$ModelCheckpoint(filepath = model_name,
                                                   save_weights_only = T,
@@ -38,11 +38,19 @@ for (seed in seedlist){
                                loss = "categorical_crossentropy",
                                metrics =  list("accuracy"))
       
-      historys[[i]] <- model |>
+      current_history <- model |>
         fit(x = dataset$train$data, 
             y = dataset$train$class,
-            epochs = epochs[i], batch_size = 32, validation_split = 0.5,
+            epochs = 50, batch_size = 128, validation_split = 0.5,
             callbacks = list(model_cp))
+      val_acc = c(val_acc, max(current_history$metrics$val_accuracy))
     }
+    current_record = c(lr, i, val_acc)
+    metric_record = rbind(metric_record, current_record)
   }
 }
+
+metric_record = data.frame(metric_record)
+colnames(metric_record) <- c('lr', 'layerid', 'acc')
+
+write.csv(metric_record, 'metric-ff1.csv')
